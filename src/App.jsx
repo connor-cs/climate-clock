b6a59aabe2c75bcbf05b6f3bdfa0fe664912d6fc
Connor <connor.cs@protonmail.com>
2023-06-18 14:37:21 -0400

    refactored

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import { getDeadlineFromAPI } from "./utils.js";
 import Clock from "./Clock.jsx";
 import "./index.css";
 import { getRemainingTime } from "./utils.js";
+import moment from "moment";
 
 const URL = "https://api.climateclock.world/v2/clock.json";
 
 export default function App() {
   const [climateCountdown, setClimateCountdown] = useState({
+    // years: timeBetween.years(),
+    // months: timeBetween.months(),
+    // days: timeBetween.days(),
+    // hours: timeBetween.hours(),
+    // minutes: timeBetween.minutes(),
+    // seconds: timeBetween.seconds(),
+    // milliseconds: timeBetween.milliseconds(),
     years: 0,
     months: 0,
     days: 0,
@@ -16,23 +24,29 @@ export default function App() {
     seconds: 0,
     milliseconds: 0,
   });
-  const [dateOfDeadline, setDateOfDeadline] = useState(0);
+  const [dateOfDeadline, setDateOfDeadline] = useState(
+    moment("2029-07-22T16:00:00+00:00")
+  );
 
   useEffect(() => {
-    getDeadlineFromAPI(URL)
-      .then((data) => data.toString())
-      .then((stringData) => setDateOfDeadline(stringData));
-
+    const timeBetween = moment.duration(dateOfDeadline.diff(moment()));
+    
     const interval = setInterval(() => {
-      updateRemainingTime(dateOfDeadline);
+      setClimateCountdown({
+        years: timeBetween.years(),
+        months: timeBetween.months(),
+        days: timeBetween.days(),
+        hours: timeBetween.hours(),
+        minutes: timeBetween.minutes(),
+        seconds: padWithZeroes(timeBetween.seconds(), 2),
+        milliseconds: padWithZeroes(timeBetween.milliseconds(), 3)
+      });
+
     }, 1);
 
     return () => clearInterval(interval);
-  }, [climateCountdown, dateOfDeadline]);
+  }, [climateCountdown]);
 
-  function updateRemainingTime(dateOfDeadline) {
-    setClimateCountdown(getRemainingTime(dateOfDeadline));
-  }
 
   return (
     <div className="main">
@@ -42,4 +56,12 @@ export default function App() {
       </div>
     </div>
   );
+
+  function padWithZeroes(input, minLength) {
+    const inputString = input.toString()
+    if (inputString.length >= minLength) return input
+    return '0'.repeat(minLength-inputString.length) + inputString
+  }
 }
+
+//"2029-07-22T16:00:00+00:00"
